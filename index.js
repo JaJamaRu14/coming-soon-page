7295846024d944b53a70d979ea9e9f537a06e884
JaJamaRu <shimizu@ray-up.com>
2021-07-09 14:18:34 +0900

    [Modified] index.html index.js style.css

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const input = document.querySelector('#email');
-const submit = document.querySelector('#submit');
-const value = input.value;
+const emailInput = document.querySelector('#email');
+const submitBtn = document.querySelector('#submit');
+const form = document.querySelector('#form');
+const alertP = document.querySelector('#alert p');
+const alertDiv = document.querySelector('#alert');
+const regex =
+  /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
 
-input.addEventListener('click', () => {
-    input.classList.add('red');
-});
+form.addEventListener('submit', alertMsg);
 
-input.addEventListener('blur', () => {
-    input.classList.remove('red')
-});
+function alertMsg(e) {
+  e.preventDefault();
 
-submit.addEventListener('click', () => {
-    if(input.value.match(regex)) {
-        console.log("OK")
-    } else {
-        addAlert();
-    }
-});
+  if (emailInput.value.match(regex)) {
+    alertP.textContent = 'Thanks for signing up!';
+    alertStyle('hsl(223, 87%, 63%)', 600);
+  } else if (emailInput.value === '') {
+    alertP.textContent = 'Whoops! It looks like you forgot to add your email';
+    alertStyle('hsl(354, 100%, 66%)', 400);
+  } else {
+    alertP.textContent = 'Please provide a valid email address';
+    alertStyle('hsl(354, 100%, 66%)', 400);
+  }
+}
 
-const regex = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/;
+function alertStyle(color, weight) {
+  alertDiv.style.visibility = 'visible';
+  alertP.style.color = color;
+  alertP.style.fontWeight = weight;
+  emailInput.style.border = `1px solid ${color}`;
+}
+
 // const emailValidation = () => {
 //     if(this.value.match) {
 //         console.log("OK")
@@ -27,15 +38,23 @@ const regex = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]
 //     }
 // }
 
-const imgSection = document.querySelector('#img-section');
-const form = document.querySelector('#form');
-function addAlert() {
-    const newDiv = document.createElement('p');
-    // const alert = document.createTextNode("Whoops! It looks like you forgot to add your email");
+// const imgSection = document.querySelector('#img-section');
+// const form = document.querySelector('#form');
+// function addAlert() {
+//   const newDiv = document.createElement('p');
+//   // const alert = document.createTextNode("Whoops! It looks like you forgot to add your email");
 
-    newDiv.innerText = "Whoops! It looks like you forgot to add your email";
-    // document.body.insertBefore(newDiv, imgSection);
-    console.log(newDiv)
-    form.appendChild(newDiv)
-}
-console.log(imgSection)
+//   newDiv.innerText = 'Whoops! It looks like you forgot to add your email';
+//   // document.body.insertBefore(newDiv, imgSection);
+//   console.log(newDiv);
+//   form.appendChild(newDiv);
+// }
+// console.log(imgSection);
+
+// emailInput.addEventListener('click', () => {
+//   emailInput.classList.add('red');
+// });
+
+// emailInput.addEventListener('blur', () => {
+//     emailInput.classList.remove('red');
+// });
